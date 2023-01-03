1ab858a2031e293c3fa550ddfd1f4e916f95e88d
mimihwa <wmfskf@naver.com>
2023-01-03 18:16:55 +0900

    230103 class

--- js/script.js
+++ js/script.js
@@ -17,18 +17,32 @@ $('.menuWrap').each(function(){
         $('.menu-area').hide();
     }); 
 
+    anchors.append('<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" focusable="false" preserveAspectRatio="xMidYMid meet" class="css-7kp13n e181xm9y0"><path xmlns="http://www.w3.org/2000/svg" d="M9.41421356,12 L15.7071068,5.70710678 C16.0976311,5.31658249 16.0976311,4.68341751 15.7071068,4.29289322 C15.3165825,3.90236893 14.6834175,3.90236893 14.2928932,4.29289322 L7.29289322,11.2928932 C6.90236893,11.6834175 6.90236893,12.3165825 7.29289322,12.7071068 L14.2928932,19.7071068 C14.6834175,20.0976311 15.3165825,20.0976311 15.7071068,19.7071068 C16.0976311,19.3165825 16.0976311,18.6834175 15.7071068,18.2928932 L9.41421356,12 Z" transform="translate(11.500000, 12.000000) scale(-1, 1) translate(-11.500000, -12.000000) "></path></svg>');
+
     anchors.mouseenter(function(){
         let currentAnchor=$(this);
         let currentPanel=$(currentAnchor.attr('data-board'));
-        lastPanel.removeClass('on');
-        currentPanel.addClass('on');
+        let windowWidth=$( window ).width()
 
-        lastAnchor=currentAnchor;
-        lastPanel=currentPanel;
+        if(windowWidth < 980){
+            PanelDiv.css('display', 'none')
+        }else{
+            lastPanel.removeClass('on');
+            currentPanel.addClass('on');
+            $(this).find('svg').css('opacity','1');
+
+            lastAnchor=currentAnchor;
+            lastPanel=currentPanel;
+        }
     });
-
+    
+    anchors.mouseleave(function(){
+        $(this).find('svg').css('opacity','0');
+    });
     tabDiv.mouseleave(function(){
         PanelDiv.removeClass('on');
+        
     });
   
 }); 
+
